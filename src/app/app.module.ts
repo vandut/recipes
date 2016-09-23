1873a1c15e95c1ac59759e4a553a8a9406370817
Konrad Bielak <konrad.bielak@gmail.com>
2016-09-23 23:53:53 +0200

    #2 Added RecipeComponent with basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
