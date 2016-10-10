8d50f1c9f0e5d82506da3a109522078306603c80
Konrad Bielak <konrad.bielak@gmail.com>
2016-10-10 21:54:42 +0200

    #6 Moved recipe objects into separate JSON asset files

--- src/app/services/populated-in-memory-recipe.service.ts
+++ src/app/services/populated-in-memory-recipe.service.ts
@@ -1,59 +1,16 @@
 import { Injectable } from '@angular/core';
 import { InMemoryRecipeService } from './in-memory-recipe.service';
 
+var recipe1 = require('../../assets/recipes/wrG2k.json');
+var recipe2 = require('../../assets/recipes/zkaJr.json');
+
 @Injectable()
 export class PopulatedInMemoryRecipeService extends InMemoryRecipeService {
 
   constructor() {
     super();
-    this.addRecipe({
-      id: 'wrG2k',
-      title: 'Pork in vindaloo sauce',
-      link: 'https://www.youtube.com/watch?v=dM8StL3nHi0',
-      portions: 3,
-      ingredients: [
-        {
-          name: 'Dried kashmiri chillies',
-          amount: 8,
-          unit: 'piece'
-        },
-        {
-          name: 'Black pepper',
-          amount: 4,
-          unit: 'seed'
-        },
-        {
-          name: 'Pork shoulder',
-          amount: 1,
-          unit: 'kg'
-        }
-      ],
-      instructions: [
-        'Cut meat into 5cm cubes',
-        'Marinate overnight (preferably 24h)'
-      ]
-    });
-    this.addRecipe({
-      id: 'zkaJr',
-      title: 'Recipe1',
-      link: 'https://www.youtube.com/watch?v=ABCDZ',
-      portions: 5,
-      ingredients: [
-        {
-          name: 'XYZ',
-          amount: 4,
-          unit: 'cup'
-        },
-        {
-          name: 'Water',
-          amount: 5.250,
-          unit: 'ml'
-        }
-      ],
-      instructions: [
-        'Abra'
-      ]
-    });
+    this.addRecipe(recipe1);
+    this.addRecipe(recipe2);
   }
 
 }
